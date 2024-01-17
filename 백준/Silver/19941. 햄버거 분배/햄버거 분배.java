8b1f2abd7e0e0075ade364534368c83a7efb9a97
Jung Kyungmin <101693680+simplist119@users.noreply.github.com>
2024-01-18 01:54:00 +0900

    [Silver III] Title: 햄버거 분배, Time: 152 ms, Memory: 14628 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -11,9 +11,12 @@ public class Main {
         int a2 = Integer.parseInt(st.nextToken());
         String str = br.readLine();
 
+        // 햄버거를 먹을 수 있는 최대 사람수는 햄버거의 갯수를 초과할 수 없다.
+        // 따라서 문자열에서 H만 조회하여 앞에서 붙어 훑는다. (P와 짝을 지어줘야함 최대 index 차이는 a2)
+        // H의 index와 P의 index를 기억한다.
         int count = 0;
         int H = 0;
-        int P = -a2; // 초기 위치를 햄버거 위치에서 a2만큼 떨어진 곳으로 설정
+        int P = -a2;
 
         while (true) {
             H = str.indexOf("H", H);
