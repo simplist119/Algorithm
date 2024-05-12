5750ccdaaf877bb7ae4fedbb63cbed41598b2554
Jung Kyungmin <101693680+simplist119@users.noreply.github.com>
2024-05-13 01:05:59 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n%2 == 1){
+            for(int i=1; i<=n; i++){
+                if(i%2 ==1) answer += i;
+            }
+        }
+        else{
+            for(int j=1; j<=n; j++){
+                if(j%2 ==0) answer += j*j;
+            }
+        }
+        return answer;
+    }
+}
