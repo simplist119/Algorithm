7b8fd3316c0bcb338c3bce9686dc91bae87d9834
Jung Kyungmin <101693680+simplist119@users.noreply.github.com>
2024-05-22 15:45:04 +0900

    [level 0] Title: l로 만들기, Time: 8.51 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : myString.toCharArray()) {
+            if(c < 'l') sb.append('l');
+            else sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
