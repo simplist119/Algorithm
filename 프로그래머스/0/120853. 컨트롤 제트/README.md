09ac1bc82f38a65a1087e923a87292bcd2f38927
Jung Kyungmin <101693680+simplist119@users.noreply.github.com>
2024-06-17 00:11:54 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.47 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/README.md
+++ 프로그래머스/0/120853. 컨트롤 제트/README.md
@@ -1,10 +1,10 @@
 # [level 0] 컨트롤 제트 - 120853 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120853) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120853?language=java) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 0.22 ms
+메모리: 69.2 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 17일 00:07:46
+2024년 06월 17일 00:11:53
 
 ### 문제 설명
 
--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,17 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
         String[] words = s.split(" ");
         int prev = 0;
         int answer = 0;
+        Stack<String> stack = new Stack<>();
 
         for (String word : words) {
-            if(word.equals("Z")) answer -= prev;
-            else {
-                answer += Integer.parseInt(word);
-                prev = Integer.parseInt(word);
-            }
+            if(word.equals("Z")) stack.pop();
+            else stack.push(word);
         }
-        
+
+        for (String string : stack) {
+            answer += Integer.parseInt(string);
+        }
+
         return answer;
     }
 }
