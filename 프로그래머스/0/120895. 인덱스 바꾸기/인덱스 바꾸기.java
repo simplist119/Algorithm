c7c9cb8944ffdec34e6ef25961ada897017709b9
Jung Kyungmin <101693680+simplist119@users.noreply.github.com>
2024-06-20 08:04:33 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] chars = my_string.toCharArray();
+
+        char temp = chars[num1];
+        chars[num1] = chars[num2];
+        chars[num2] = temp;
+
+        my_string = String.valueOf(chars);
+        return my_string;
+    }
+}
